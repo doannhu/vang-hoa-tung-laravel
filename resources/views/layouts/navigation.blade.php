1d23c48f63dda57d1fe351f210dfe70007bb4eba
doannhu <82296094+doannhu@users.noreply.github.com>
2025-06-01 07:41:25 +1000

    update navigation link

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,16 +5,28 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('admin.management') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
-                    </a>
+                    @if(auth()->user()->role === 'admin')
+                        <a href="{{ route('admin.management') }}">
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        </a>
+                    @else
+                        <a href="{{ route('admin.page_management') }}">
+                            <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        </a>
+                    @endif
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('admin.management')" :active="request()->routeIs('admin.management')">
-                        {{ __('Quản lý hệ thống') }}
-                    </x-nav-link>
+                    @if(auth()->user()->role === 'admin')
+                        <x-nav-link :href="route('admin.management')" :active="request()->routeIs('admin.management')">
+                            {{ __('Quản lý hệ thống') }}
+                        </x-nav-link>
+                    @else
+                        <x-nav-link :href="route('admin.page_management')" :active="request()->routeIs('admin.page_management')">
+                            {{ __('Quản lý trang') }}
+                        </x-nav-link>
+                    @endif
                 </div>
             </div>
 
