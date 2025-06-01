9401fca5970b80238ac965294841d1df4461ff39
doannhu <82296094+doannhu@users.noreply.github.com>
2025-06-01 12:12:39 +1000

    clear unused functions: email verification, password reset

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,35 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Database\Seeder;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('seed:all', function () {
+    $this->info('Starting to seed all data...');
+    
+    // Run all seeders in the correct order
+    $this->call('db:seed', [
+        '--class' => 'Database\\Seeders\\AdminUserSeeder'
+    ]);
+    
+    $this->call('db:seed', [
+        '--class' => 'Database\\Seeders\\BlogPostSeeder'
+    ]);
+    
+    $this->call('db:seed', [
+        '--class' => 'Database\\Seeders\\CatalogueSeeder'
+    ]);
+    
+    $this->call('db:seed', [
+        '--class' => 'Database\\Seeders\\FeaturedProductSeeder'
+    ]);
+    
+    $this->call('db:seed', [
+        '--class' => 'Database\\Seeders\\PromotionSeeder'
+    ]);
+    
+    $this->info('All seeders have been executed successfully!');
+})->purpose('Run all database seeders in the correct order');
